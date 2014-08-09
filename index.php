2a16da3c2e03e297ac7fdd6903592631d5511586
Robb Lewis <me@therobb.com>
2014-08-09 17:04:13 +0100

    Added pagination

--- index.php
+++ index.php
@@ -13,6 +13,15 @@
 		endwhile;
 	endif;
 
+	echo '<div class="pagination">';
+	$prev = get_previous_posts_link();
+	echo $prev;
+
+	$next = get_next_posts_link();
+	echo '<span style="float:right">'.$next.'</span>';
+
+	echo '</div>';
+
 	get_footer();
 
 ?>
